cafdf7a5222ee7fc378c7fd0df06e0b58cf7cc6c
MUSKAN <muskanprajapati0609@gmail.com>
2024-06-19 14:33:39 +0530

    assignment

--- day10_assign/keyboard_dd/ktimer.c
+++ day10_assign/keyboard_dd/ktimer.c
@@ -4,7 +4,7 @@
 #include <linux/interrupt.h>
 #include <linux/delay.h>
 #include <asm/io.h>
-#include<linux/ioport.h>
+#include <linux/ioport.h>
 
 struct timer_list mytimer;
 int ticks;
@@ -14,9 +14,13 @@ int ticks;
 void mytimer_function(struct timer_list *ptimer)
 {
 	printk(KERN_INFO " %s : mytimer_function \n", THIS_MODULE->name);
-
+	
+	while (inb(KBD_CONTROL_REG) & 0x02)
+		mdelay(1);
 	outb(0xAE, KBD_CONTROL_REG);
 	mod_timer(&mytimer, jiffies + ticks);
+
+	printk(KERN_INFO " %s : keyboard enabled\n", THIS_MODULE->name);
 }
 
 static __init int desd_init(void)
@@ -24,11 +28,15 @@ static __init int desd_init(void)
 	int sec = 10;
 	ticks = sec * HZ;		//750
 
+	printk(KERN_INFO " %s : Timer initialisation\n", THIS_MODULE->name);
 	timer_setup(&mytimer, mytimer_function, 0);
 	mytimer.expires = jiffies + ticks;
 	add_timer(&mytimer);
 	
+	while (inb(KBD_CONTROL_REG) & 0x02)
+		mdelay(1);
 	outb(0xAD, KBD_CONTROL_REG);
+	printk(KERN_INFO " %s : keyboard disabled\n", THIS_MODULE->name);
 
 	printk(KERN_INFO " %s : Timer initialisation is done successfully\n", THIS_MODULE->name);
 	return 0;
